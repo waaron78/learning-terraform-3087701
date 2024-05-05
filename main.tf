3bd4676aec206ce69eb867c1deae77ab24def764
waaron78 <41174163+waaron78@users.noreply.github.com>
2024-05-05 17:14:15 -0400

    Update security group

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "blog_sg" {
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egressgress_rules = ["all-all"]
+  egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
