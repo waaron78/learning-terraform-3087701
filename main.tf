18b5d27c2f79f2f253242039eb0596379d966ac3
waaron78 <41174163+waaron78@users.noreply.github.com>
2024-05-06 15:09:38 -0400

    Create New Dev VPC

--- main.tf
+++ main.tf
@@ -17,11 +17,31 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+ 
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
 vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -31,6 +51,8 @@ vpc_security_group_ids = [module.blog_sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
+
+  vpc_id = module.blog_vpc.vpc_id
   name = "blog"
 
   vpc_id = data.aws_vpc.default.id
